cd2b9727dfba78c723d9b6a8f247fca5d073b187
mickey61295 <j.maihby@gmail.com>
2022-11-01 12:03:13 +0530

    Reusable bull

---
+++ src/common/Bull.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const Bull = () => (
+	<Box
+		component="span"
+		sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+	>
+		•
+	</Box>
+)
+
+export default Bull
