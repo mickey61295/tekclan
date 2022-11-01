bf681964f74248cb9a65d14ee3fb94efb96e8d96
mickey61295 <j.maihby@gmail.com>
2022-11-01 12:02:41 +0530

    HeaderButtons

--- src/common/HeaderButtons.jsx
+++ src/common/HeaderButtons.jsx
@@ -1,26 +1,28 @@
 import {
 	Button,
-	Divider,
 	FormControl,
 	InputLabel,
 	Select,
 	Typography,
 } from '@mui/material'
-import { borderLeft } from '@mui/system'
 import React from 'react'
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
 const headersList = ['Overview', 'Views', 'Applied']
 
 const HeaderButtons = () => {
+	const theme = useTheme()
+	const minWidth = useMediaQuery(theme.breakpoints.up('md'))
 	return (
 		<>
 			{headersList.map((item) => (
-				<Button>
+				<Button key={item.toLocaleLowerCase()}>
 					<Typography variant="subtitle1" fontSize={15} color="grey.500">
 						{item}
 					</Typography>
 				</Button>
 			))}
-			<FormControl
+			{minWidth && <FormControl
 				variant="standard"
 				sx={{ minWidth: '80px', p: 0, verticalAlign: 'bottom' }}
 			>
@@ -36,7 +38,7 @@ const HeaderButtons = () => {
 					label={'Oct 19 - Oct 25'}
 					disabled
 				></Select>
-			</FormControl>
+			</FormControl>}
 		</>
 	)
 }
