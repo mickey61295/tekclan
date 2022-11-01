023bfe0430549202ea2a9eb63064d9455ba271da
mickey61295 <j.maihby@gmail.com>
2022-11-01 11:59:46 +0530

    Updated with responsive sidebar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { withStyles } from '@mui/styles'
 import {
+	Avatar,
 	Badge,
 	Button,
 	FormControl,
@@ -9,10 +10,16 @@ import {
 	Select,
 	Typography,
 } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { green, deepOrange, grey } from '@mui/material/colors'
+import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded'
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded'
 import AddIcon from '@mui/icons-material/Add'
 import ListItem from '../common/ListItem'
 import dashboard from '../constants/Dashboard'
+import ResponsiveDrawer from './Drawer'
 
 const styles = () => ({
 	leftContainer: {
@@ -41,6 +48,14 @@ const styles = () => ({
 
 const Dashboard = (props) => {
 	const { classes } = props
+	const [open, setOpen] = useState(false)
+	const theme = useTheme()
+	const minWidth = useMediaQuery(theme.breakpoints.up('md'))
+
+	const handleOpen = () => {
+		setOpen(true)
+	}
+
 	return (
 		<Grid
 			container
@@ -49,12 +64,47 @@ const Dashboard = (props) => {
 				background:
 					'linear-gradient(176deg, rgb(253, 253, 253) 0%, rgb(241, 239, 226) 100%)',
 
-				height: '11rem',
+				height: minWidth ? '11rem' : '23rem',
 				'& hr:last-child': {
 					display: 'none',
 				},
 			}}
 		>
+			{!minWidth && (
+				<Grid
+					container
+					item
+					alignItems="center"
+					justifyContent={'flex-start'}
+					sx={{ padding: '10px' }}
+				>
+					<MenuRoundedIcon
+						sx={{ width: 30, height: 30, cursor: 'pointer' }}
+						color="action"
+						onClick={handleOpen}
+					/>
+					<ResponsiveDrawer open={open} setOpen={setOpen} />
+					<Grid
+						item
+						sx={{ flex: 1, justifyContent: 'center', display: 'flex' }}
+					>
+						<Avatar
+							sx={{
+								width: 50,
+								height: 50,
+								bgcolor: green[500],
+								cursor: 'pointer',
+							}}
+						>
+							<AssignmentRoundedIcon />
+						</Avatar>
+					</Grid>
+					<NotificationsNoneIcon
+						sx={{ width: 30, height: 30, cursor: 'pointer' }}
+						color="action"
+					/>
+				</Grid>
+			)}
 			{/* Top Part */}
 			<Grid container item md={6} className={classes.leftContainer}>
 				<Grid item>
@@ -74,18 +124,20 @@ const Dashboard = (props) => {
 					</FormControl>
 				</Grid>
 			</Grid>
-			<Grid container item md={6} className={classes.rightContainer}>
-				<Grid item>
-					<Badge badgeContent={4} color="primary">
-						<NotificationsNoneIcon color="action" />
-					</Badge>
+			{minWidth && (
+				<Grid container item md={6} className={classes.rightContainer}>
+					<Grid item>
+						<Badge badgeContent={4} color="primary">
+							<NotificationsNoneIcon color="action" />
+						</Badge>
+					</Grid>
+					<Grid item>
+						<Button variant="contained" startIcon={<AddIcon />}>
+							Post a Job
+						</Button>
+					</Grid>
 				</Grid>
-				<Grid item>
-					<Button variant="contained" startIcon={<AddIcon />}>
-						Post a Job
-					</Button>
-				</Grid>
-			</Grid>
+			)}
 			<Grid
 				container
 				item
@@ -95,7 +147,7 @@ const Dashboard = (props) => {
 					width: '95%',
 					margin: 'auto',
 					borderRadius: '10px',
-					height: '3.5rem',
+					// height: '3.5rem',
 				}}
 			>
 				{dashboard.map((eachItem, idx) => (
