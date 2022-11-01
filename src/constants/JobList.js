334a6ac6fbb4b5b34fae1dcf9d9721e6d3c1d4ea
mickey61295 <j.maihby@gmail.com>
2022-11-01 11:59:15 +0530

    StandAlone Datas

---
+++ src/constants/JobList.js
@@ -0,0 +1,22 @@
+const jobList = [
+	{
+		'Brand Designer': {
+			jobType: 'Full-Time',
+			jobCategory: 'Design',
+			company: 'Revolut',
+			location: 'London, United Kingdom',
+			diff: 6,
+		},
+	},
+	{
+		'Interactive Developer': {
+			jobType: 'Full-Time',
+			jobCategory: 'Development',
+			company: 'Revolut',
+			location: 'London, United Kingdom',
+			diff: 4,
+		},
+	},
+]
+
+export default jobList
---
+++ src/constants/Jobs.js
@@ -0,0 +1,24 @@
+const jobs = [
+	{
+		color: '#00FF00',
+		text: 'Full Time',
+		number: 37,
+	},
+	{
+		color: '#AAFF00',
+		text: 'Part Time',
+		number: 40,
+	},
+	{
+		color: '#FFAA00',
+		text: 'Remote',
+		number: 30,
+	},
+	{
+		color: '#FF0000',
+		text: 'Internship',
+		number: 56,
+	},
+]
+
+export default jobs
